ade7d7744c0af5db5d70b41e73959d701d730c0c
samael <mrrobot7380@gmail.com>
2024-09-26 23:02:12 +0300

    Added Search based on Category

--- Components/MovieCard/MovieCard.jsx
+++ Components/MovieCard/MovieCard.jsx
@@ -1,34 +1,36 @@
 import PropTypes from "prop-types";
 
-const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+const MoviesByCategory = ({
+  movie: { id, original_title, release_date, poster_path },
+}) => {
+  const posterUrl = poster_path
+    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+    : "https://via.placeholder.com/400";
+
   return (
-    <div className="movie" key={imdbID}>
+    <div className="movie" key={id}>
       <div>
-        <p>{Year}</p>
+        <p>{release_date}</p>
       </div>
 
       <div>
-        <img
-          src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"}
-          alt={Title}
-        />
+        <img src={posterUrl} alt={original_title} />
       </div>
 
       <div>
-        <span>{Type}</span>
-        <h3>{Title}</h3>
+        <h3>{original_title}</h3>
       </div>
     </div>
   );
 };
 
-MovieCard.propTypes = {
+MoviesByCategory.propTypes = {
   movie: PropTypes.shape({
-    imdbID: PropTypes.string.isRequired,
-    Year: PropTypes.string.isRequired,
-    Poster: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Type: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired, // Change to number
+    release_date: PropTypes.string.isRequired,
+    poster_path: PropTypes.string,
+    original_title: PropTypes.string.isRequired,
   }).isRequired,
 };
-export default MovieCard;
+
+export default MoviesByCategory;
--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,44 @@ import MovieCard from "../Components/MovieCard/MovieCard.jsx";
 import Navbar from "../Components/Navbar/Navbar.jsx";
 
 import "./App.css";
-const apiUrl = import.meta.env.VITE_API_URL;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 import SearchIcon from "/eglass.svg";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [message, setMessage] = useState("Search for movies");
 
   const searchMovies = async (title) => {
     try {
-      const response = await fetch(`${apiUrl}&s=${title}`);
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${title}`,
+      );
       if (!response.ok) {
         throw new Error("Request Failed");
       }
 
       const data = await response.json();
-      console.log(data);
-      setMovies(data.Search);
+      setMovies(data.results);
+      if (movies.length === 0) {
+        setMessage("No movies found");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const fetchMoviesByCategory = async (categoryId) => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${categoryId}`,
+      );
+      if (!response.ok) {
+        throw new Error("Retrieving Categories failed");
+      }
+      const data = await response.json();
+      setMovies(data.results);
     } catch (err) {
       console.error(err);
     }
@@ -28,7 +48,7 @@ const App = () => {
 
   return (
     <>
-      <Navbar />
+      <Navbar onCategorySelect={fetchMoviesByCategory} />
 
       <div className="app">
         <h1>Movies</h1>
@@ -49,12 +69,12 @@ const App = () => {
         {movies?.length > 0 ? (
           <div className="container">
             {movies.map((movie) => (
-              <MovieCard key={movie.imdbID} movie={movie} />
+              <MovieCard key={movie.id} movie={movie} />
             ))}
           </div>
         ) : (
           <div className="empty">
-            <h2>Search more movies...</h2>
+            <h2>{message}</h2>
           </div>
         )}
       </div>
